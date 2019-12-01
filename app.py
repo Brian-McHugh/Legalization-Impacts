3e53abf7d4013b8477db80e7961e274cc97e622a
dabustillossorelle <danielle.sorelle@gmail.com>
2019-12-01 09:44:19 -0600

    update

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
-useyr_891617 = Base.classes.useyr_891617
-arrest_prispop_data = Base.classes.arrest_prispop_data
+State_Metadata = Base.classes.useyr_891617
+State_Arrest = Base.classes.arrest_prispop_data
 
 
 @app.route("/")
@@ -42,27 +42,27 @@ def names():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(useyr_891617).statement
+    stmt = db.session.query(State_Arrest).statement
     df = pd.read_sql_query(stmt, db.session.bind)
 
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns)[2:])
 
 
-@app.route("/metadata/<State>")
-def useyr_891617(State):
+@app.route("/metadata/<state>")
+def useyr_891617(state):
     """Return the MetaData for a given sample."""
     sel = [
-        useyr_891617.State,
-        useyr_891617.age1217_89,
-        useyr_891617.age1217_1617,
-        useyr_891617.age1825_89,
-        useyr_891617.age1825_1617,
-        useyr_891617.age26_89,
-        useyr_891617.age26_1617,
+        State_Metadata.State,
+        State_Metadata.age1217_89,
+        State_Metadata.age1217_1617,
+        State_Metadata.age1825_89,
+        State_Metadata.age1825_1617,
+        State_Metadata.age26_89,
+        State_Metadata.age26_1617,
     ]
 
-    results = db.session.query(*sel).filter(useyr_891617.State == State).all()
+    results = db.session.query(*sel).filter(State_Metadata.state == state).all()
 
     # Create a dictionary entry for each row of metadata information
     useyr_891617 = {}
@@ -80,20 +80,26 @@ def useyr_891617(State):
     return jsonify(useyr_891617)
 
 
-@app.route("/arrest_prispop_data/<arrest_prispop_data>")
-def arrest_prispop_data(arrest_prispop_data):
+@app.route("/arrest_prispop_data/<state>")
+def arrest_prispop_data(state):
     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(arrest_prispop_data).statement
+    stmt = db.session.query(State_Arrest).statement
     df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Filter the data based on the sample number and
+    # only keep rows with values above 1
+    state_data = df.loc[df[state] > 1, ["dt_ids", "dt_information", state]]
+
+    # Sort by sample
+    state_data.sort_values(by=state, ascending=False, inplace=True)
 
     # Format the data to send as json
     data = {
-        "dt_ids": arrest_prispop_data.dt_id.values.tolist(),
-        "sample_values": arrest_prispop_data[sample].values.tolist(),
-        "dt_information": arrest_prispop_data.dt_information.tolist(),
+        "dt_ids": state_data.dt_id.values.tolist(),
+        "sample_values": state_data[state].values.tolist(),
+        "dt_information": state_data.dt_information.tolist(),
     }
     return jsonify(data)
-
 
 if __name__ == "__main__":
     app.run()
